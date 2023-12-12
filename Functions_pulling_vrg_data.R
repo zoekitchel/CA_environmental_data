1140d0cb33ac42b3ada31b5c9caf6412247972e0
zoekitchel <zoe.j.kitchel@gmail.com>
2023-12-12 15:24:25 -0800

    small edits to function sourcing data

--- Functions_pulling_vrg_data.R
+++ Functions_pulling_vrg_data.R
@@ -1,92 +1,170 @@
 #note, you can only have one true at a time (only return one file)
-pull_VRG_data_files <- function(system = c("macos","windows","custom_path"), newest = T, event = F, fish = F, swath = F, UPC = F, ISC = F){
+#note, that all archived files MUST be in 'Old Versions' file for the following code to work
+
+pull_VRG_data_files <- function(newest = T, #do you want newest version of data file (T) or list of all options (F)
+                                event = F, #do you want raw event data?
+                                fish = F, #do you want raw fish observation data?
+                                swath = F, #do you want raw swath data?
+                                UPC = F, #do you want raw UPC data?
+                                UPC_format = NA, #do you want UPC data in pisco format or vrg format? c("pisco","vrg")
+                                ISC = F, #do you want raw ISC data?
+                                all_sites = F, #do you want all sites table?
+                                all_fish = F, #do you want all fish table?
+                                benthic_reef_spp = F #do you want benthic reef species table?
+){
   
-if(system == "macos"){  
-  #Link to VRG Files dropbox
-
-VRG_files <- file.path(path.expand("~"),"Dropbox","VRG Files")
-VRG_files <- sub("/Documents", "", VRG_files)
-}
-
-if(system == "windows"){ #this needs to be checked on a windows
-  VRG_files <- file.path(path.expand("~"),"Dropbox","VRG Files")
-  VRG_files <- sub("/Documents", "", VRG_files)       
-}
+  #identify dropbox path
+  dropbox_path <- here("Dropbox")
   
-if(system == "custom_path"){
-  x <- readline("What is your file path to dropbox? (i.e /Users/santaclaus/Dropbox/ or C::)")
-}
-
-############################################################################
-#Event data
-############################################################################
-if(event == T){
-  VRG_integrated_event_files <- list.files(path = VRG_files, recursive = T, pattern = "Integrated Event Data.*\\.csv")
-
-if(newest == T){
-VRG_integrated_event_files <- VRG_integrated_event_files[!grepl("Old Versions",VRG_integrated_event_files)]
-
-VRG_integrated_event <- fread(file.path(VRG_files,VRG_integrated_event_files))
-
-return(VRG_integrated_event)
-}else{return(VRG_integrated_event_files)}
-
-  }
-############################################################################
-#Fish data
-############################################################################
-if (fish == T){
-  VRG_integrated_fish_files <- list.files(path = VRG_files, recursive = T, pattern = "Integrated Fish Data .*\\.csv")
-
-if(newest == T){
-  VRG_integrated_fish_files <- VRG_integrated_fish_files[!grepl("Old Versions",VRG_integrated_fish_files)]
+  #VRG Files
+  VRG_files <- "VRG Files"
   
-  VRG_integrated_fish <- fread(file.path(VRG_files,VRG_integrated_fish_files))  
-  return(VRG_integrated_fish)
-}else{return(VRG_integrated_fish_files)}
+  #full path name
+  VRG_files_full_path <- file.path(dropbox_path, VRG_files)
   
-  }
-############################################################################
-#UPC data
-############################################################################
-if (UPC == T){
-  VRG_integrated_UPC_files <- list.files(path = VRG_files, recursive = T, pattern = "Integrated VRG UPC Data .*\\.csv")
-
-if(newest == T){
-  VRG_integrated_UPC_files <- VRG_integrated_UPC_files[!grepl("Old Versions",VRG_integrated_UPC_files)]
   
-  VRG_integrated_UPC <- fread(file.path(VRG_files,VRG_integrated_UPC_files))
-  return(VRG_integrated_UPC)
-}else(return(VRG_integrated_UPC_files))
-
-  }
-############################################################################
-#Swath data
-############################################################################
-if (swath == T){
-  VRG_integrated_swath_files <- list.files(path = VRG_files, recursive = T, pattern = "Integrated Swath Data .*\\.csv")
   
-  if(newest == T){
-    VRG_integrated_swath_files <- VRG_integrated_swath_files[!grepl("Old Versions",VRG_integrated_swath_files)]
-    VRG_integrated_swath <- fread(file.path(VRG_files,VRG_integrated_swath_files))  
-    return(VRG_integrated_swath)
-  }else{return(VRG_integrated_swath_files)}
-  
-  }
-############################################################################
-#ISC data
-############################################################################
-if (event == T){
-  VRG_integrated_ISC_files <- list.files(path = VRG_files, recursive = T, pattern = "Integrated ISC Data .*\\.csv")
-  
-  if(newest == T){
+  ############################################################################
+  #Event data
+  ############################################################################
+  if(event == T){
+    VRG_integrated_event_files <- list.files(path = VRG_files_full_path, recursive = T, pattern = "Integrated Event Data.*\\.csv")
     
-    VRG_integrated_ISC_files <- VRG_integrated_ISC_files[!grepl("Old Versions",VRG_integrated_ISC_files)]
+    if(newest == T){
+      VRG_integrated_event_files <- VRG_integrated_event_files[!grepl("Old Versions",VRG_integrated_event_files)]
+      
+      VRG_integrated_event <- fread(file.path(VRG_files_full_path,VRG_integrated_event_files))
+      
+      return(VRG_integrated_event)
+    }else{return(VRG_integrated_event_files)}
     
-    VRG_integrated_ISC <- fread(file.path(VRG_files,VRG_integrated_ISC_files))  
-    return(VRG_integrated_ISC)
+  }
+  ############################################################################
+  #Fish data
+  ############################################################################
+  if (fish == T){
+    VRG_integrated_fish_files <- list.files(path = VRG_files_full_path, recursive = T, pattern = "Integrated Fish Data .*\\.csv")
     
-  }else{return(VRG_integrated_ISC_files)}
+    if(newest == T){
+      VRG_integrated_fish_files <- VRG_integrated_fish_files[!grepl("Old Versions",VRG_integrated_fish_files)]
+      
+      VRG_integrated_fish <- fread(file.path(VRG_files_full_path,VRG_integrated_fish_files))  
+      return(VRG_integrated_fish)
+    }else{return(VRG_integrated_fish_files)}
+    
+  }
+  ############################################################################
+  #UPC data
+  ############################################################################
+  if (UPC == T & is.na(UPC_format)){
+    print("Please specify UPC_format as `pisco` or `vrg` in function arguments")
+  }
   
+  
+  if (UPC == T & UPC_format %in% c("vrg", "VRG")){ #VRG format
+    VRG_integrated_UPC_files <- list.files(path = VRG_files_full_path, recursive = T, pattern = "Integrated VRG UPC Data .*\\.csv")
+    
+    if(newest == T){
+      VRG_integrated_UPC_files <- VRG_integrated_UPC_files[!grepl("Old Versions",VRG_integrated_UPC_files)]
+      
+      VRG_integrated_UPC <- fread(file.path(VRG_files_full_path,VRG_integrated_UPC_files))
+      return(VRG_integrated_UPC)
+    }else(return(VRG_integrated_UPC_files))
+    
+  }
+  
+  if (UPC == T & UPC_format %in% c("pisco", "PISCO")){ #PISCO format
+    VRG_integrated_UPC_files <- list.files(path = VRG_files_full_path, recursive = T, pattern = "Integrated UPC PISCO Data .*\\.csv")
+    
+    if(newest == T){
+      VRG_integrated_UPC_files <- VRG_integrated_UPC_files[!grepl("Old Versions",VRG_integrated_UPC_files)]
+      
+      VRG_integrated_UPC <- fread(file.path(VRG_files_full_path,VRG_integrated_UPC_files))
+      return(VRG_integrated_UPC)
+    }else(return(VRG_integrated_UPC_files))
+    
+  }
+  ############################################################################
+  #Swath data
+  ############################################################################
+  if (swath == T){
+    VRG_integrated_swath_files <- list.files(path = VRG_files_full_path, recursive = T, pattern = "Integrated Swath Data .*\\.csv")
+    
+    if(newest == T){
+      VRG_integrated_swath_files <- VRG_integrated_swath_files[!grepl("Old Versions",VRG_integrated_swath_files)]
+      VRG_integrated_swath <- fread(file.path(VRG_files_full_path,VRG_integrated_swath_files))  
+      return(VRG_integrated_swath)
+    }else{return(VRG_integrated_swath_files)}
+    
+  }
+  ############################################################################
+  #ISC data
+  ############################################################################
+  if (event == T){
+    VRG_integrated_ISC_files <- list.files(path = VRG_files_full_path, recursive = T, pattern = "Integrated ISC Data .*\\.csv")
+    
+    if(newest == T){
+      
+      VRG_integrated_ISC_files <- VRG_integrated_ISC_files[!grepl("Old Versions",VRG_integrated_ISC_files)]
+      
+      VRG_integrated_ISC <- fread(file.path(VRG_files_full_path,VRG_integrated_ISC_files))  
+      return(VRG_integrated_ISC)
+      
+    }else{return(VRG_integrated_ISC_files)}
+    
+  }
+  
+  
+  ###########################################################################
+  #All sites (ALL_Sites_)
+  ###########################################################################
+  if (all_sites == T){
+    VRG_all_sites_files <- list.files(path = VRG_files_full_path, recursive = T, pattern = "ALL_Sites_.*\\.csv")
+    
+    if(newest == T){
+      
+      VRG_all_sites_files <-VRG_all_sites_files[!grepl("Old Versions",VRG_all_sites_files)]
+      
+      VRG_all_sites <- fread(file.path(VRG_files_full_path,VRG_all_sites_files))
+      return(VRG_all_sites)
+      
+    }else{return(VRG_all_sites_files)}
+    
+  }
+  ############################################################################
+  #All fish (Species.Length.Weight.Guild.)
+  ############################################################################
+  if (all_fish == T){
+    VRG_all_fish_files <- list.files(path = VRG_files_full_path, recursive = T, pattern = "Species.Length.Weight.Guild.*\\.csv")
+    
+    if(newest == T){
+      #exclude strings
+      exclude_strings <- c("Old Versions", "Latitude Integration", "Platform Project")
+      pattern <- paste0(exclude_strings, collapse = "|")
+      
+      VRG_all_fish_files <-VRG_all_fish_files[!grepl(pattern,VRG_all_fish_files)]
+      
+      VRG_all_fish <- fread(file.path(VRG_files_full_path,VRG_all_fish_files))
+      return(VRG_all_fish)
+      
+    }else{return(VRG_all_fish_files)}
+    
+  }
+  
+  ############################################################################
+  #Benthic reef species (BRS ONLY)
+  ############################################################################
+  if (benthic_reef_spp == T){
+    VRG_BRS_files <- list.files(path = VRG_files_full_path, recursive = T, pattern = "BRS ONLY.*\\.csv")
+    
+    if(newest == T){
+      
+      VRG_BRS_files <-VRG_BRS_files[!grepl("Old Versions",VRG_BRS_files)]
+      
+      VRG_BRS <- fread(file.path(VRG_files_full_path,VRG_BRS_files))  
+      return(VRG_BRS)
+      
+    }else{return(VRG_BRS_files)}
+    
   }
 }
