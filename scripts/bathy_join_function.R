cb3d2b548514b4ea70dfd91c243622565a2386a8
zoekitchel <zoe.j.kitchel@gmail.com>
2023-12-14 12:17:53 -0800

    updated and added to local dropbox files

--- scripts/bathy_join_function.R
+++ scripts/bathy_join_function.R
@@ -5,7 +5,7 @@
 
 #Zoë J. Kitchel
 #Created 13 December 2023
-#Modified 13 December 2023
+#Modified 14 December 2023
 ##############################################################################
 #There are multiple options for where to get bathymetry data:
 #method = "ETOPO": Marmap package (but our sites may be too close to shore for this to work, example below anyway though)
@@ -16,27 +16,21 @@
 ##############################################################################
 
 #you will need the following packages loaded
-# library(sf)
-# library(data.table)
-# library(ggplot2)
-# library(dplyr)
-# library(maps)
-# library(raster)
-# library(rnaturalearth)
-# library(viridis)
-# library(RColorBrewer)
-# library(marmap) #to pull depth data
-# library(rasterVis)
+ # library(sf)
+ # library(data.table)
+ # library(dplyr)
+ # library(raster)
+ # library(marmap) #to pull depth data
 
 #lat_lon input must be data frame with lat and lon in Longitude and Latitude columns, okay if there are other columns
 
-get_depth <- function(lat_lon, ETOPO = T, CDFW = T, USGS_socal=T, dist_200m = F){ #default will give you all three added to your table
-  
-  lat_lon_only <- data.table(lat_lon)
+add_depth_columns <- function(lat_lon, ETOPO = F, CDFW = F, USGS_socal=F, dist_200m = F){ 
   
   if(!("Longitude" %in% colnames(lat_lon)) | !("Latitude" %in% colnames(lat_lon))){
     stop("Are you sure you have Longitude and Latitude columns in this dataframe?")
   }
+  
+  lat_lon_only <- data.table(lat_lon)
   
   lat_lon_only <- unique(lat_lon_only[,.(Longitude, Latitude)])
   
@@ -44,9 +38,15 @@ get_depth <- function(lat_lon, ETOPO = T, CDFW = T, USGS_socal=T, dist_200m = F)
 
     #set square from which to extract bathy data from NOAA server
     marmap_bathy <- getNOAA.bathy(min(lat_lon_only$Longitude), max(lat_lon_only$Longitude), min(lat_lon_only$Latitude), max(lat_lon_only$Latitude), resolution = 0.000001) #bathymetry matrix
-    lat_lon_only[,ETOPODepth := get.depth(marmap_bathy, lat_lon_only, locator=F)$depth]
+  
+    #make new copy of lat lon only to add new column to so no repeats if we have T for multiple bathy types
+    lat_lon_only.c <- copy(lat_lon_only)
     
-    lat_lon_bathy <- lat_lon[lat_lon_only, on = c("Longitude","Latitude"), all = T]
+    #get depth info for specified lat lon coordinates
+    lat_lon_only.c[,ETOPODepth := get.depth(marmap_bathy, lat_lon_only.c, locator=F)$depth]
+    
+    #merge with main 'return' output
+    lat_lon_bathy <- lat_lon[lat_lon_only.c, on = c("Longitude","Latitude"), all = T]
     
   }
   
@@ -76,15 +76,24 @@ get_depth <- function(lat_lon, ETOPO = T, CDFW = T, USGS_socal=T, dist_200m = F)
     
       lat_lon_only.t <- st_transform(lat_lon_only.sf, crs = crs_Raster) #convert to raster projection for extraction
     
-    #match sites to depths
-      lat_lon_only[, CDFW_200m:= extract(bathy_200m, lat_lon_only.t)]
+      #make new copy of lat lon only to add new column to so no repeats if we have T for multiple bathy types
+      lat_lon_only.c <- copy(lat_lon_only)
       
-      #link up
-      lat_lon_bathy <- lat_lon[lat_lon_only, on = c("Longitude","Latitude"), all = T]
-    
+    #match sites to depths
+      lat_lon_only.c[, CDFW_200m:= extract(bathy_200m, lat_lon_only.t)]
+      
+      #link up (either to other bathy if it exists or just with lat lon coordinates)
+        #merge with main 'return' output
+      if(exists("lat_lon_bathy")){
+      lat_lon_bathy <- lat_lon_bathy[lat_lon_only.c, on = c("Longitude","Latitude"), all = T]
+      }else{
+      lat_lon_bathy <- lat_lon[lat_lon_only.c, on = c("Longitude","Latitude"), all = T]
+      }
   }
   
   if(dist_200m == T){
+    
+    options(timeout=1000) #allow more time to download large rasters
     
     temp <- tempdir()
     
@@ -94,21 +103,38 @@ get_depth <- function(lat_lon, ETOPO = T, CDFW = T, USGS_socal=T, dist_200m = F)
     #unzip file
     unzip(file.path(temp,"contours_5m.zip"), exdir = temp)
     
-    CA_contours_5m <- read_sf(file.path("raw_data","contours_5m","contours_5m.shp"))
+    CA_contours_5m <- read_sf(file.path(temp,"contours_5m.shp"))
     
     CA_200m_contour <- CA_contours_5m %>% filter(CONTOUR == -200)
-    CA_200m_contour.t <- st_transform(CA_200m_contour, crs = crs(VRG_lat_lon_all.sf)) #match crs
     CA_0m_contour <- CA_contours_5m %>% filter(CONTOUR == 0)
     
-    #calculate distance of point to each of 37 spatial features in CA_200m_contour.t
-    VRG_site_dist_200m <- data.table(st_distance(VRG_lat_lon_all.sf, CA_200m_contour.t))
+    #convert points to sf with non-projected crs (must do first)
+    lat_lon_only.sf <- st_as_sf(lat_lon_only, coords = c("Longitude", "Latitude"),
+                                crs = 4326) # WGS84 - a good default for unprojected coords)
+    
+    lat_lon_only.t <- st_transform(lat_lon_only.sf, crs = crs(CA_200m_contour)) #convert to raster projection for extraction
+    
+    #calculate distance of point to each of 37 spatial features in CA_200m_contour
+    site_dist_200m <- data.table(st_distance(lat_lon_only.t, CA_200m_contour))
     
     #we only want closest value (minimum distance)
     # Find the minimum distance for each row
-    VRG_site_dist_200m_min <- apply(VRG_site_dist_200m, 1, min)
+    site_dist_200m_min <- apply(site_dist_200m, 1, min)
     
-    #add back to point dt
-    VRG_lat_lon_all.r[,dist_200m_bath := VRG_site_dist_200m_min]
+    #make new copy of lat lon only to add new column to so no repeats if we have T for multiple bathy types
+    lat_lon_only.c <- copy(lat_lon_only)
+    
+    #match sites to distances
+    lat_lon_only.c[, dist_200m_bath:= site_dist_200m_min]
+    
+    #link up (either to other bathy if it exists or just with lat lon coordinates)
+      #merge with main 'return' output
+    if(exists("lat_lon_bathy")){
+      lat_lon_bathy <- lat_lon_bathy[lat_lon_only.c, on = c("Longitude","Latitude"), all = T]
+    }else{
+      lat_lon_bathy <- lat_lon[lat_lon_only.c, on = c("Longitude","Latitude"), all = T]
+    }
+    
     
   }
   
@@ -116,14 +142,93 @@ get_depth <- function(lat_lon, ETOPO = T, CDFW = T, USGS_socal=T, dist_200m = F)
     
     options(timeout=1000) #allow more time to download large rasters
     
+      #download key (where is what ID)
+      
+      # Define the URL of the zip file
+      zip_url <- "https://pubs.usgs.gov/ds/487/data/DEMCoverageAreas/DEMCoverageAreas.zip"
+      
+      # Create a temporary directory
+      temp_dir <- tempdir()
+      
+      # Create a temporary file to save the downloaded zip file
+      zip_file <- file.path(temp_dir, "temp_download.zip")
+      
+      # Download the zip file
+      download.file(zip_url, destfile = zip_file, mode = "wb")
+      
+      # Extract the contents of the zip file to the temporary directory
+      unzip(zip_file, exdir = temp_dir)
     
+      #make simple feature 
+      coverage_area <- read_sf(file.path(temp_dir,"DEMCoverageAreas.shp"))
+      
+      #transform lat lon to match CRS of reference shapefile
+      lat_lon_only.sf <- st_as_sf(lat_lon_only, coords = c("Longitude", "Latitude"), crs = 4326) %>% 
+        st_transform(crs = crs(coverage_area))
+      
+      #add other lat lon values from previous projection
+      lat_lon_only.sf$Latitude <- lat_lon_only$Latitude
+      lat_lon_only.sf$Longitude <- lat_lon_only$Longitude
+      
+      #which lat long points link to which relevant DEM boundaries (DEM_ID)
+      
+      lat_lon_DEM_link <- st_intersection(lat_lon_only.sf, coverage_area)
+      
+      #DEM_ID, put into single vector
+      DEM_IDs <- levels(factor(lat_lon_DEM_link$DEM_ID))
+      
+      #convert to lower
+      DEM_to_download <- tolower(levels(factor(lat_lon_DEM_link$DEM_ID)))
+      
+      #setup data table to take stats
+      highres_socal <- data.table()
+      
+      #download DEMs one by one to current working directory (temporarily! these are large)
+      for (i in 1:length(DEM_to_download)) {
+        #relevant lat_lons
+        lat_lon_DEM_link.subset <- lat_lon_DEM_link %>% filter(DEM_ID == DEM_IDs[i])
+        
+        file_url <- paste0("https://pubs.usgs.gov/ds/487/data/DEMs/",DEM_to_download[i],".zip")
+        
+        #download file to working directory
+        download.file(file_url,paste0(DEM_to_download[i],".zip")) #this will vary in time, some up to ~200 MB!
+        
+        #unzip file
+        unzip(paste0(DEM_to_download[i],".zip"))
+        
+        #remove zip file, only keep la10.txt
+        file.remove(paste0(DEM_to_download[i],".zip"))
+        
+        #load up as raster
+        DEM_single_raster <- raster(paste0(DEM_to_download[i],".txt"))
+        
+        #set CRS
+        utm_crs <- "+proj=utm +zone=11 +datum=WGS84 +units=m +no_defs" #from technical documentation
+        projection(DEM_single_raster) <- utm_crs
+        
+        #subset data table
+        highres_socal.subset <- data.table(Longitude = lat_lon_DEM_link.subset$Longitude,
+                                           Latitude = lat_lon_DEM_link.subset$Latitude,
+                                           highrez_depth = raster::extract(DEM_single_raster,  lat_lon_DEM_link.subset))   #extract relevant lat lon values
+        
+        #remove raster file
+        file.remove(paste0(DEM_to_download[i],".txt"))
+        
+        #set up datatable
+        highres_socal <- rbind(highres_socal, highres_socal.subset)
+      }
+      
+      #link up (either to other bathy if it exists or just with lat lon coordinates)
+       #merge with main 'return' output
+      if(exists("lat_lon_bathy")){
+        lat_lon_bathy <- lat_lon_bathy[highres_socal, on = c("Longitude","Latitude"), all = T]
+      }else{
+        lat_lon_bathy <- lat_lon[highres_socal, on = c("Longitude","Latitude"), all = T]
+      }
+      
+      
+      
+    }
     
-    
-    
-    
-    
-    
-  }
-  
   return(lat_lon_bathy)
 }
